e3da26ea0aaca2c5650b3cdb8ed93495689bc1bd
johnshields <G00348436@gmit.ie>
2021-01-26 23:33:08 +0000

    Orpheus attacks Xanthos when in range

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # The Battle - AR Scene
 An experimental Augmented Reality Scene that is being developed with `Unity` and `C#`.
 
-# Development Enviroment
+# Development Environment
 * OS Name: <br>
 `Microsoft Windows 10`
 * Unity Version: <br>
@@ -10,31 +10,32 @@ An experimental Augmented Reality Scene that is being developed with `Unity` and
 `Rider - 2020.3.2`
 
 # Scene
-3D, Fantasy Game that it is in a 2D like view...
+3D, Fantasy Game that it is in a 2D like view... <br>
+Play as the Paladin known as `Xanthos` and battle against `Orpheus` the Dark Wizard!
 
 ## Preview
-`Xanthos` is out for a battle...  <br><br>
+Xanthos is out for a battle...  <br><br>
 ![Game Scene](https://github.com/johnshields/TheBattle-AR-Scene/blob/main/preview/scene_v1.gif)
-                           
-Video Source: Took it myself at a old, local monastary. <br>
-See the monastary -> [Kilmacduagh Abbey](http://monastic.ie/history/kilmacduagh/).
+
+Video Source: Took it myself at a old, local monastery. <br>
+See the monastery -> [Kilmacduagh Abbey](http://monastic.ie/history/kilmacduagh/).
 
 Characters are from [Mixamo](https://www.mixamo.com/#/).
 
 # Characters
 ## Xanthos :crossed_swords:
-* Knight
+* Paladin
   - Sword & Shield
 
 ## Orpheus :fire:
-* Wizard
+* Dark Wizard
   - Magic
 
 # Gameplay
 ## Controls :running:
 `A` & `D` or :arrow_left: & :arrow_right:
 to move Xanthos `LEFT` and `RIGHT`. <br>
-`LEFT SHIFT` 
+`LEFT SHIFT`
 to make Xanthos `SWING` his `SWORD`.
 
 ***
--- theBattle/Assets/Characters/Orpheus/OrpheusController.cs
+++ theBattle/Assets/Characters/Orpheus/OrpheusController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 /*
  * John Shields
@@ -13,41 +11,97 @@ namespace Characters.Orpheus
 {
     public class OrpheusController : MonoBehaviour
     {
-        public NavMeshAgent orpheusAgent;
+        // Xanthos
         public Transform xanthosPlayer;
-        
+        public LayerMask xanthosMask;
+
+        // Orpheus
+        public NavMeshAgent orpheusAgent;
+
         // animation 
         private Animator _animator;
         private int _idleActive;
         private int _walkActive;
-        
+        private int _attackActive;
+
+        // walking
+        private bool _walkPointSet;
+
+        // attacking
+        public float timeBetweenAttacks;
+        private bool _alreadyAttacked;
+        public GameObject projectile;
+
+        // states
+        public float sightRange, attackRange;
+        public bool playerInSightRange, playerInAttackRange;
+
         private void Start()
         {
             _animator = GetComponent<Animator>();
 
             _idleActive = Animator.StringToHash("IdleActive");
             _walkActive = Animator.StringToHash("WalkActive");
+            _attackActive = Animator.StringToHash("AttackActive");
         }
-        
+
         private void FixedUpdate()
         {
-            Follow();
+            var position = transform.position;
+            playerInSightRange = Physics.CheckSphere(position, sightRange, xanthosMask);
+            playerInAttackRange = Physics.CheckSphere(position, attackRange, xanthosMask);
+
+            if (playerInSightRange && !playerInAttackRange) Follow();
+            if (playerInAttackRange && playerInSightRange) Attack();
         }
 
         private void Follow()
         {
+            // trigger walk animation
             if (orpheusAgent.SetDestination(xanthosPlayer.position))
             {
                 _animator.SetBool(_walkActive, true);
                 _animator.SetBool(_idleActive, false);
+                _animator.SetBool(_attackActive, false);
             }
             else
             {
                 _animator.SetBool(_walkActive, false);
                 _animator.SetBool(_idleActive, true);
+                _animator.SetBool(_attackActive, false);
             }
         }
         
+        private void Attack()
+        {
+            // Make sure Orpheus doesn't move
+            // and have him look at Xanthos
+            orpheusAgent.SetDestination(transform.position);
+            transform.LookAt(xanthosPlayer);
+
+            switch (_alreadyAttacked)
+            {
+                case false:
+                {
+                    // attack
+                    _animator.SetBool(_walkActive, false);
+                    _animator.SetBool(_idleActive, false);
+                    _animator.SetBool(_attackActive, true);
+                    var magicFire = Instantiate(projectile, transform.position, Quaternion.identity)
+                        .GetComponent<Rigidbody>();
+                    magicFire.AddForce(transform.forward * 32f, ForceMode.Impulse);
+                    magicFire.AddForce(transform.up * 8f, ForceMode.Impulse);
+                    
+                    _alreadyAttacked = true;
+                    Invoke(nameof(ResetAttack), timeBetweenAttacks);
+                    break;
+                }
+            }
+        }
+
+        private void ResetAttack()
+        {
+            _alreadyAttacked = false;
+        }
     }
-    
 }
