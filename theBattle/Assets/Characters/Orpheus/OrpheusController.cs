410b63f7647b0e9bdb4d29dcb86f1c80ba031cdf
johnshields <G00348436@gmit.ie>
2021-01-26 22:44:13 +0000

    completely redid Orpheus
    Orpheus' controller is now much more simple - need to have him attack Xanthos when in range...

--- theBattle/Assets/Characters/Orpheus/OrpheusController.cs
+++ theBattle/Assets/Characters/Orpheus/OrpheusController.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 /*
  * John Shields
@@ -13,108 +13,41 @@ namespace Characters.Orpheus
 {
     public class OrpheusController : MonoBehaviour
     {
-       public NavMeshAgent agent;
-
-    public Transform player;
-
-    public LayerMask whatIsGround, whatIsPlayer;
-
-    public float health;
-
-    // Patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    // Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
-    public GameObject projectile;
-
-    // States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
-    {
-        player = GameObject.Find("Xanthos").transform;
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void Update()
-    {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patrolling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
-    }
-
-    private void Patrolling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        // walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-    }
-    private void SearchWalkPoint()
-    {
-        // Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
-    }
-
-    private void ChasePlayer()
-    {
-        agent.SetDestination(player.position);
-    }
-
-    private void AttackPlayer()
-    {
-        // Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
+        public NavMeshAgent orpheusAgent;
+        public Transform xanthosPlayer;
+        
+        // animation 
+        private Animator _animator;
+        private int _idleActive;
+        private int _walkActive;
+        
+        private void Start()
         {
-            // Attack code here
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            // End of attack code
+            _animator = GetComponent<Animator>();
 
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            _idleActive = Animator.StringToHash("IdleActive");
+            _walkActive = Animator.StringToHash("WalkActive");
         }
-    }
-    private void ResetAttack()
-    {
-        alreadyAttacked = false;
-    }
+        
+        private void FixedUpdate()
+        {
+            Follow();
+        }
 
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+        private void Follow()
+        {
+            if (orpheusAgent.SetDestination(xanthosPlayer.position))
+            {
+                _animator.SetBool(_walkActive, true);
+                _animator.SetBool(_idleActive, false);
+            }
+            else
+            {
+                _animator.SetBool(_walkActive, false);
+                _animator.SetBool(_idleActive, true);
+            }
+        }
+        
     }
-    private void DestroyEnemy()
-    {
-        Destroy(gameObject);
-    }
-    }
+    
 }
